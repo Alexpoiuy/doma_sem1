83e8e14b2ffe200be5e0fcf44e75801bbb22bd5b
Alexpoiuy <129396501+Alexpoiuy@users.noreply.github.com>
2023-04-14 01:15:45 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,72 +1,97 @@
-﻿/*
- Задача 2: Напишите программу, которая на вход принимает два числа 
- и выдаёт, какое число большее, а какое меньшее.
-*/
+﻿// программа на вход принимает трёхзначное число и на выходе показ вторую цифру
 /*
-Console.Write("Input a first number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-
-int max = num;
-
-if(num > num2)
+int Prompt(string message)
 {
-    Console.WriteLine(max = num);
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
 }
-else
+
+int number = Prompt("Введите трёхзначное число: ");
+if (number < 100 || number >= 1000)
 {
-    Console.WriteLine(max = num2);
+    Console.WriteLine("Вы ввели не трёхзначное число, поэжалуйста повторите ввод: ");
+    return;
+}
+
+Console.WriteLine($"Введёное число {number} ");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondRank} ");
+*/
+//программа выводит 3 цифру заданного числа или сообщает что ее нет
+/*
+int Prompt(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+ink GetThirdRank(int number)
+{
+    while(number > 999)
+    {
+        number /= 10
+    }
+    return number % 10;
+}
+
+bool ValidateNumber(int number)
+{
+    if (number < 100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+        return false;
+    }
+    returne true;
+}
+
+int number = Prompt ("Введите число: ");
+if (ValidateNumber(number))
+{
+    Console.WriteLine(GetThirdRank(number));
 }
 */
-// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//программа принимает на вход цифру и проверяет выходной ли это
 /*
-Console.Write("Input a first number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a second number: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Input a third number: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
-
-int max = num;
-
-if(num > max) max = num;
-if(num2 > max) max = num2;
-if(num3 > max) max = num3;
-
-Console.Write("max = ");
-Console.WriteLine(max);
-*/
-/*
- Задача 6: Напишите программу, которая на вход принимает число и выдаёт, 
-является ли число чётным (делится ли оно на два без остатка).
-*/
-/*
-Console.Write("Input a  number: ");
-int num = Convert.ToInt32(Console.ReadLine());
-
-if(num % 2 == 0)
+int Prompt(string message)
 {
-    Console.WriteLine("Чётное");
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
 }
-else
+
+bool Isweekend(int weekDay);
 {
-    Console.WriteLine("Не чётное");
+    if (weekDay > 5);
+    {
+        return true;
+    }
+    return false;
 }
-*/
-/*
-Задача 8: Напишите программу, которая на вход принимает число (N),
- а на выходе показывает все чётные числа от 1 до N.
-*/
-/*
-Console.Write("Input a positive number: ");
-int num = Convert.ToInt32(Console.ReadLine());
 
-int current = 2;
-
-while (current <= num)
+bool ValidateWeekday(int number);
 {
-    Console.Write(current+ "");
-    current = current + 2;
+    if (number > 0 && num <=7)
+    {
+        return true;
+    }
+    Console.WriteLine("Это не день недели");
+    return false;
+}
+
+int weekDay = Prompt("Введите номер дня недели (от 1 до7): ");
+if (ValidateWeekday(weekDay))
+{
+    if (weekend(numday))
+    {
+        Console.WriteLine("Выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Будний день!");
+    }
 }
 */
